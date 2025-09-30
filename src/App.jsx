53999b22c8a2d97a20c5eacd680f26784d433295
Tijani Basit <tijanibwebdev@gmail.com>
2025-09-30 03:37:32 -0700

    new push

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,7 @@
 import { useEffect, useState } from "react";
-// import Dashboard from "./components/Dashboard";
-import './styles/dashboard.css';
-import './styles/weatherCard.css';
-import './styles/spinner.css';
 import WeatherWrapper from "./components/WeatherWrapper";
 function App() {
   return (
-    //  <Dashboard />
     <WeatherWrapper/>
   );
 }
